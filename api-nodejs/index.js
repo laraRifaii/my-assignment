45118ad0b408468629f7f8b015268a0cf6c0f434
laraRifaii <rifailara33@gmail.com>
2026-05-27 08:47:29 +0300

    Added Cors Policy for my front end access

--- api-nodejs/index.js
+++ api-nodejs/index.js
@@ -2,10 +2,16 @@
 require("dotenv").config();
 
 const express = require("express");
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 app.use(express.json());
+
+
 
 const PORT = process.env.PORT || 3000;
 const NODE_ENV = process.env.NODE_ENV;
